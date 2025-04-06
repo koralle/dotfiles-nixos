6b8fc67bddebea085c9e86ee6d5056145283ed7d
koralle <mkhd7930@gmail.com>
2025-04-06 20:49:52 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "My configurations of NixOS";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nur = {
+      url = "github:nix-community/NUR";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, nur, ... }@inputs: 
+  let
+    overlays = [ nur.overlays.default ];
+    pkgs = import nixpkgs { inherit overlays; };
+  in {
+    formatter = pkgs.nixfmt-rfc-style;
+
+    nixosConfigurations = {
+      koralle = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [];
+      };
+    };
+  };
+}
