472e0d1293b79b7b87cdd9cba44565bc4ab8f26b
koralle <koralle@koralle-mgmg.com>
2025-06-18 01:37:30 +0900

    feat(home-manager/mise): add `config.toml`

--- users/koralle/home.nix
+++ users/koralle/home.nix
@@ -6,6 +6,11 @@
   home.homeDirectory = "/home/koralle";
   home.stateVersion = "24.05";
 
+  home.file.${config.xdg.configHome} = {
+    source = ./.config;
+    recursive = true;
+  };
+
   home.packages = with pkgs; [
     fish
     
