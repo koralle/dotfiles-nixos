d51eadf880e026922d5e51e4fd3cf686debaa713
koralle <koralle@koralle-mgmg.com>
2025-06-18 12:54:59 +0900

    feat(home-manager/neovim): add keymaps of tabpage

---
+++ users/koralle/.config/nvim/lua/configs/keymaps.lua
@@ -0,0 +1,10 @@
+-- tabpage
+vim.keymap.set("n", "<space>tn", ":$tabnew<CR>", { noremap = true })
+vim.keymap.set("n", "<space>tc", ":tabclose<CR>", { noremap = true })
+vim.keymap.set("n", "<C-j>", ":tabp<CR>", { noremap = true })
+vim.keymap.set("n", "<C-k>", ":tabn<CR>", { noremap = true })
+-- move current tab to previous position
+vim.keymap.set("n", "<space>tmp", ":-tabmove<CR>", { noremap = true })
+-- move current tab to next position
+vim.keymap.set("n", "<space>tmn", ":+tabmove<CR>", { noremap = true })
+
