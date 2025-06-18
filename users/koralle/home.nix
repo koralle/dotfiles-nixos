461a0923aa64c2bd5c76ff42ccce4b380e58d581
koralle <koralle@koralle-mgmg.com>
2025-06-18 23:46:30 +0900

    feat(nixos): update stateVersion to 25.05

--- users/koralle/home.nix
+++ users/koralle/home.nix
@@ -4,7 +4,7 @@
  
   home.username = "koralle";
   home.homeDirectory = "/home/koralle";
-  home.stateVersion = "24.05";
+  home.stateVersion = "25.05";
 
   home.file.${config.xdg.configHome} = {
     source = ./.config;
