88b83e3b8bcd1dd20a9538c8e4860ba3094adae6
koralle <koralle@koralle-mgmg.com>
2025-06-18 11:42:38 +0900

    feat(home-manager/neovim): setup `tabby.nvim`

---
+++ users/koralle/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  -- Ref: https://github.com/nvim-lualine/lualine.nvim
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      -- Ref: https://github.com/catppuccin/nvim?tab=readme-ov-file#integrations
+      theme = "catppuccin",
+    },
+  },
+}
