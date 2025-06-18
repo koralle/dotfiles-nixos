715515cf04d580392159e8bc7cde10c268525a3d
koralle <koralle@koralle-mgmg.com>
2025-06-18 21:23:48 +0900

    feat(home-manager/mise): integrate Fish-Shell

--- users/koralle/.config/fish/config.fish
+++ users/koralle/.config/fish/config.fish
@@ -1,3 +1,11 @@
 # Initialize Starship
 # https://starship.rs/ja-JP/guide/
 starship init fish | source
+
+# Initialize mise
+# https://mise.jdx.dev/getting-started.html#_2a-activate-mise
+if status is-interactive
+  mise activate fish | source
+else
+  mise activate fish --shims | source
+end
