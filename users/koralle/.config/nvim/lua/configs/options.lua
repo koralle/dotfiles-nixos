c1dd6bd73c1ec14431f6c5f8c081cb7ed21ebae6
koralle <koralle@koralle-mgmg.com>
2025-06-19 00:47:48 +0900

    feat(neovim): add `denops-shared-server`

--- users/koralle/.config/nvim/lua/configs/options.lua
+++ users/koralle/.config/nvim/lua/configs/options.lua
@@ -1,5 +1,6 @@
 -- global variables
 vim.g.mapleader = " "
+vim.g.denops_server_addr = '127.0.0.1:32123'
 
 -- global
 vim.opt.termguicolors = true
--- users/koralle/.config/nvim/lua/plugins/denops.lua
+++ users/koralle/.config/nvim/lua/plugins/denops.lua
@@ -5,6 +5,12 @@ local spec = {
     lazy = false,
     priority = 1000,
   },
+  {
+    "vim-denops/denops-shared-server.vim",
+    name = "denops-shared-server",
+    lazy = false,
+    priority = 1000,
+  },
 }
 
 return spec
