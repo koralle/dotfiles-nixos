f23d48b7a11a47af6847b40ba7b2591d61488b91
koralle <koralle@koralle-mgmg.com>
2025-06-20 22:55:54 +0900

    feat(nixos): resolve deprecated expr

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -46,12 +46,15 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
+
+  services.desktopManager.gnome = {
+    enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
