8f45b79434cdecaec49e8dc16dafefaa1c9a590e
koralle <koralle@koralle-mgmg.com>
2025-06-20 22:25:38 +0900

    feat(nixos): set `allowUnfree`

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -172,6 +172,8 @@
     moralerspace-hwjpdoc
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
